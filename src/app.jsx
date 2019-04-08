d06b68827805c14ee08a0923f8f196160e27c825
Karo Kuusikallio <karo.kuusikallio@avanade.com>
2019-04-08 16:06:17 +0300

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SpotifyWebApi from "spotify-web-api-js";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const spotifyApi = new SpotifyWebApi();
+
+class SpotCoverApp extends React.Component {
+    constructor() {
+        super();
+        const params = this.getHashParams();
+        const token = params.access_token;
+        if (token) {
+            spotifyApi.setAccessToken(token);
+        }
+        this.state = {
+            loggedIn: token ? true : false,
+            nowPlaying: { name: 'Not Checked', albumArt: '' }
+        }
+    }
+
+    getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        e = r.exec(q)
+        while (e) {
+            hashParams[e[1]] = decodeURIComponent(e[2]);
+            e = r.exec(q);
+        }
+        return hashParams;
+    }
+
+    getNowPlaying() {
+        spotifyApi.getMyCurrentPlaybackState()
+            .then((response) => {
+                this.setState({
+                    nowPlaying: {
+                        name: response.item.name,
+                        albumArt: response.item.album.images[0].url
+                    }
+                });
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <a href='http://localhost:1410' > Login to Spotify </a>
+                <br />
+                <button onClick={() => {
+                    this.getNowPlaying()
+                }}>
+                    Get Track Played Now
+                </button>
+                <br />
+                <div>
+                    Now Playing: {this.state.nowPlaying.name}
+                </div>
+                <div>
+                    <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<SpotCoverApp />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.jsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: "./public",
+    hot: true
+  }
+};
