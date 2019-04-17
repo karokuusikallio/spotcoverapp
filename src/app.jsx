7af7ddb69c0d3341861e5ce3691fb667a4bf391d
Karo Kuusikallio <karo.kuusikallio@avanade.com>
2019-04-17 15:17:37 +0300

    amount of images in a row selection

--- src/app.jsx
+++ src/app.jsx
@@ -24,11 +24,19 @@ class SpotCoverApp extends React.Component {
             artistName: "",
             albumName: "",
             albumYear: "",
-            albumPlayLink: ""
+            albumPlayLink: "",
+            imageSizing: 2,
+            randomSearch: false
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
         this.showAlbumInfo = this.showAlbumInfo.bind(this);
+        this.handleImageSizing = this.handleImageSizing.bind(this);
+        this.handleRandomSearch = this.handleRandomSearch.bind(this);
+    }
+
+    componentDidMount() {
+        this.handleRandomSearch();
     }
 
     getHashParams() {
@@ -50,23 +58,34 @@ class SpotCoverApp extends React.Component {
     }
 
     handleSearch(event) {
-        event.preventDefault();
-        let albumImageArray = [];
-        console.log(this.state.query);
-        spotifyApi.searchAlbums(this.state.query).then(results => {
-            console.log(results);
-            results.albums.items.map(item => {
-                let albumItem = [];
+        if (event) {
+            event.preventDefault();
+        }
 
-                if (item.images.length > 0) {
-                    albumItem.push(item.id);
-                    albumItem.push(item.images[0].url);
-                    albumImageArray.push(albumItem);
-                }
-            })
+        let albumImageArray = [];
+
+        spotifyApi.searchAlbums(this.state.query).then(results => {
+
+            if (this.state.randomSearch && results.albums.items.length < 4) {
+                this.handleRandomSearch();
+                return;
+            }
+            else {
+                results.albums.items.map(item => {
+
+                    let albumItem = [];
+
+                    if (item.images.length > 0) {
+                        albumItem.push(item.id);
+                        albumItem.push(item.images[0].url);
+                        albumImageArray.push(albumItem);
+                    }
+                })
+            }
         }).then(() => {
             this.setState({
-                albumUrls: albumImageArray
+                albumUrls: albumImageArray,
+                showInfo: false
             })
         }
         ).catch(error => {
@@ -77,6 +96,25 @@ class SpotCoverApp extends React.Component {
                 console.log("Http Request Error:");
                 console.log(error);
             }
+        })
+    }
+
+    handleRandomSearch() {
+        const randomWord = require('random-words');
+        const searchWord = randomWord();
+
+        this.setState({
+            query: searchWord,
+            randomSearch: true
+        }, () => {
+            this.handleSearch();
+            this.search.value = "";
+        });
+    }
+
+    handleImageSizing(event) {
+        this.setState({
+            imageSizing: event.target.value
         })
     }
 
@@ -109,9 +147,11 @@ class SpotCoverApp extends React.Component {
     }
 
     render() {
+        let imageSizing = `PerRow${this.state.imageSizing}`;
+
         let albumImageRender = [];
         this.state.albumUrls.map(album => {
-            albumImageRender.push(<img src={album[1]} className="singleImage" key={album[0]} onClick={e => this.showAlbumInfo(album[0])} />)
+            albumImageRender.push(<img src={album[1]} className={`singleImage ${imageSizing}`} key={album[0]} onClick={e => this.showAlbumInfo(album[0])} />)
         });
 
         return (
@@ -120,16 +160,38 @@ class SpotCoverApp extends React.Component {
 
                     <SpotCoverLogo height={90} width={180} className="spotcoverlogo" />
 
-                    <form onSubmit={this.handleSearch} className="albumSearch">
-                        <p>Discover Album Cover Art</p>
-                        <input type="text" placeholder="Search..." ref={input => this.search = input} onChange={this.handleInputChange} autoFocus />
-                        <button type="submit"><span>Search from Spotify</span></button>
-                    </form>
-
+                    <div className="headerRight">
+                        <form onSubmit={this.handleSearch} className="albumSearch">
+                            <p>Discover Album Cover Art from Spotify</p>
+                            <input type="text" placeholder="Search..." ref={input => this.search = input} onChange={this.handleInputChange} autoFocus />
+                            <button type="submit" className="spotButton" onClick={() => {
+                                this.setState({
+                                    randomSearch: false
+                                })
+                            }}><span>Let's Go!</span></button>
+                        </form>
+                        <button onClick={this.handleRandomSearch} className="spotButton"><span>Random Search</span></button>
+                    </div>
                 </div>
 
                 <div className="imageContainer">
                     {albumImageRender}
+                </div>
+
+                <div className="footer">
+                    <div className="footerText perRowSelection">
+                        <p>Images Per Row</p>
+                        <select value={this.state.imageSizing} onChange={this.handleImageSizing}>
+                            <option value={2}>2</option>
+                            <option value={3}>3</option>
+                            <option value={4}>4</option>
+                        </select>
+                    </div>
+
+                    <div className="footerText">
+                        <p>2019 © Karo Kuusikallio</p>
+                    </div>
+
                 </div>
 
                 {this.state.showInfo &&
