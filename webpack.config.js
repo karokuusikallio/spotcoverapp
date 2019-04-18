bc27744ad80dc4b832dc2bbfa4821fe8114450cd
Karo Kuusikallio <karo.kuusikallio@avanade.com>
2019-04-18 13:53:38 +0300

    setting up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 const path = require("path");
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    })
+  ],
   entry: "./src/app.jsx",
   output: {
     path: path.resolve(__dirname, "public"),
@@ -16,8 +28,13 @@ module.exports = {
         }
       },
       {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        test: /\.(sa|sc|c)ss$/,
+        use: [{
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.svg$/,
